23ff47bf12c0d56f4ae4f14ddfb15a36e18a44e9
LilwenQN <lilwenqn@gmail.com>
2026-03-17 15:42:27 +0000

    Set up Vehicle class

--- src/vehicle.py
+++ src/vehicle.py
@@ -1,4 +1,10 @@
+from abc import abstractmethod
 
 class Vehicle:
-    def __init__(self):
+    def __init__(self, reg: str, weight: float):
+        self.RegistrationNumber = reg
+        self.Weight = weight
+    
+    @abstractmethod
+    def CalculateFee(self):
         pass
